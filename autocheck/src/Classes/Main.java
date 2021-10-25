27010c7372008ee668f2af53f0e6622dc7dde593
noje_ <noje_i@yahoo.com>
2021-10-25 13:43:33 +0300

    commit

--- autocheck/src/Classes/Main.java
+++ autocheck/src/Classes/Main.java
@@ -3,5 +3,6 @@ package Classes;
 public class Main {
     public static void main(String[] args) {
         System.out.println("noje a scris asta!");
+        System.out.println("toni se misca ca bunamea");
     }
 }
