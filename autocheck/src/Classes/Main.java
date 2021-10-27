b621704e37dda547f200087da6aa1483ba8ff3ea
IancuMilu <iancu.milu@gmail.com>
2021-10-27 12:49:11 +0300

    Delete

--- autocheck/src/Classes/Main.java
+++ autocheck/src/Classes/Main.java
@@ -2,28 +2,7 @@ package Classes;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("noje a scris asta!");
-        System.out.println("asdaghf");
-        System.out.println("mesaj ca sa vad la mera");
-        System.out.println("milu iancu aici");
-        System.out.println("alt mesaj de la milu");
 
-
-        System.out.println("Mera a fost aici");
-        System.out.println("Iancu test");
-        System.out.println("Iancu test2");
-
-
-
-
-
-
-
-
-
-
-
-
-        System.out.println("noje a scris pe linia 27");
+        System.out.println("Proiect3");
     }
 }
