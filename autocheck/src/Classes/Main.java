d52293fde39d5e3cff61e10989450b4eb9ea9140
IancuMilu <91599880+IancuMilu@users.noreply.github.com>
2021-10-27 13:19:32 +0300

    Update Main.java

--- autocheck/src/Classes/Main.java
+++ autocheck/src/Classes/Main.java
@@ -4,22 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("noje a scris asta!");
         System.out.println("asdaghf");
-        System.out.println("mesaj ca sa vad la mera");
-        System.out.println("milu iancu aici");
-        System.out.println("alt mesaj de la milu");
-
-
-        System.out.println("Mera a fost aici");
-        System.out.println("Iancu test");
-        System.out.println("Iancu test2");
-
-
-
-
-
-
-
-
+      
 
 
 
