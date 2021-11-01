ecfb26b11dc31b74c22eb1a78ccd232f85c20e03
Toni <antonio.moldovan82@yahoo.com>
2021-11-01 15:29:42 +0200

    created Welcome Frame

--- autocheck/src/GUI/Welcome.java
+++ autocheck/src/GUI/Welcome.java
@@ -1,7 +1,29 @@
 package GUI;
 
+import javax.swing.*;
+
 public class Welcome {
-    public static void main(String[] args) {
-        System.out.println("noje a fost aici");
+
+    private final int WIDTH = 350;
+    private final int HEIGHT = 600;
+
+    //Empty Constructor
+    public Welcome() {
+
+    }
+
+    //Main Constructor
+    public Welcome(String title) {
+
+        JFrame frame = new JFrame();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("AutoCheck " + title);
+        frame.setResizable(false);
+        frame.setLayout(null);
+
+        frame.setSize(WIDTH, HEIGHT);
+
+        frame.setVisible(true);
     }
 }
