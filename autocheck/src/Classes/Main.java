8f32b096bec4466311ce34462e1f7da157897f24
Alex <meraalex2001@gmail,com>
2021-11-01 15:42:20 +0200

    Added Java connecter for MySQL database. with hook exeption

--- autocheck/src/Classes/Main.java
+++ autocheck/src/Classes/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test");
 
+        System.out.println("Hello there!!!!");
+
         int x = 3;
 
     }
