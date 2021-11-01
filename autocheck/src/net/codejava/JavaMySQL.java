9c3bee72f845984d048eab0ca168149311affe67
Alex <meraalex2001@gmail,com>
2021-11-01 16:20:31 +0200

    Trying to push br:mera to main br

---
+++ autocheck/src/net/codejava/JavaMySQL.java
@@ -0,0 +1,34 @@
+package net.codejava;
+
+import java.sql.*;
+
+public class JavaMySQL {
+    public static void main(String[] args){
+
+        String url = "jdbc:mysql://localhost:3306/autocheck";
+        String username="root";
+        String password="root";
+
+        try {
+            Connection connection= DriverManager.getConnection(url,username,password);
+            System.out.println("Database connected successfully...");
+
+            String sql="INSERT INTO Users (username,password) VALUES (?,?)";
+            PreparedStatement statement=connection.prepareStatement(sql);
+            int rows= statement.executeUpdate();
+            if(rows>0){
+                System.out.println("A row has been inserted...");
+            }
+
+            statement.close();
+            connection.close();
+
+        }catch (SQLException e){
+            System.out.println("Eroare...");
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
