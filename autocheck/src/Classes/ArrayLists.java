462fb3b8251ebbb436aca36e4f37516fb33dc4cc
IancuMilu <iancu.milu@gmail.com>
2021-11-02 19:41:01 +0200

    verificare utilizator si parola din bd

--- autocheck/src/Classes/ArrayLists.java
+++ autocheck/src/Classes/ArrayLists.java
@@ -2,11 +2,10 @@ package Classes;
 
 import java.util.ArrayList;
 
-public class ArrayLists
-{
-    public static ArrayList<Masini> date = new ArrayList<>();
-    public static ArrayList<User> users = new ArrayList<>();
-    public static ArrayList<Mechanic> machanics = new ArrayList<>();
+public class ArrayLists{
+
+    public static ArrayList<Masini> masini = new ArrayList<>();
+
 
 
 
