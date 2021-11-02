6a328981071323d5e0645fd76e892b1cf3368472
Alex <meraalex2001@gmail,com>
2021-11-02 18:39:45 +0200

    added j driver for DB connection. DB can connect to java program. added People.txt for reference later...

--- autocheck/src/net/codejava/JavaMySQL.java
+++ autocheck/src/net/codejava/JavaMySQL.java
@@ -13,6 +13,7 @@ public class JavaMySQL {
             Connection connection= DriverManager.getConnection(url, username, password);
             System.out.println("Database connected successfully...");
 
+
             //DB operations
 
             String sql="INSERT INTO Users (username,password) VALUES (?,?)";
