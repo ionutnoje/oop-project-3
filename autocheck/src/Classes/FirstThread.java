6fad4e2355f5b83bf374173482deeb121e3df78e
IancuMilu <iancu.milu@gmail.com>
2021-11-02 18:36:54 +0200

    created threads

---
+++ autocheck/src/Classes/FirstThread.java
@@ -0,0 +1,9 @@
+package Classes;
+
+public class FirstThread implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
--- autocheck/src/Classes/Main.java
+++ autocheck/src/Classes/Main.java
@@ -2,9 +2,17 @@ package Classes;
 
 import GUI.Welcome;
 
-public class Main {
+public class Main extends Thread{
     public static void main(String[] args) {
 
+
         Welcome welcomeFrame = new Welcome("Login");
+
+        FirstThread firstThread = new FirstThread();
+        SecondThread secondThread = new SecondThread();
+
+        Thread thread1 = new Thread(firstThread);
+        Thread thread2 = new Thread(secondThread);
+
     }
 }
