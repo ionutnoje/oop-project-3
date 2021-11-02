e5ece5155f758a3782893b65626f6520fc751ffc
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-11-02 19:05:18 +0200

    classe created arraylists

--- autocheck/src/Classes/Main.java
+++ autocheck/src/Classes/Main.java
@@ -7,10 +7,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Masini> arrayList_masini = new ArrayList<>();
-
-
-
 
         Welcome welcomeFrame = new Welcome("Login");
     }
