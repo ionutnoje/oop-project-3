9bd21072edb7905cdd5474be06229557fa9330d4
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-11-02 18:36:28 +0200

    classe created arraylists

---
+++ autocheck/src/Classes/ArrayLists.java
@@ -0,0 +1,13 @@
+package Classes;
+
+import java.util.ArrayList;
+
+public class ArrayLists
+{
+    public static ArrayList<Masini> date = new ArrayList<>();
+    public static ArrayList<User> users = new ArrayList<>();
+    public static ArrayList<Mechanic> machanics = new ArrayList<>();
+
+
+
+}
--- autocheck/src/Classes/Main.java
+++ autocheck/src/Classes/Main.java
@@ -2,8 +2,15 @@ package Classes;
 
 import GUI.Welcome;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Masini> arrayList_masini = new ArrayList<>();
+
+
+
 
         Welcome welcomeFrame = new Welcome("Login");
     }
