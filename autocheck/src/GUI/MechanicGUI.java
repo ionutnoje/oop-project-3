66f55abe5ce5b52d8c72697334c1c5c01a06f559
Toni <antonio.moldovan82@yahoo.com>
2021-11-02 22:34:49 +0200

    finished front-end

--- autocheck/src/GUI/MechanicGUI.java
+++ autocheck/src/GUI/MechanicGUI.java
@@ -35,6 +35,8 @@ public class MechanicGUI implements ActionListener {
     public JButton refreshButton;
     public JButton exitButton;
 
+    public int carIdToCheck;
+
     //Empty constructor
     public MechanicGUI(){
 
@@ -95,22 +97,109 @@ public class MechanicGUI implements ActionListener {
         finished.setForeground(yellowColor);
         finished.setBounds(900, 80, 200, 40);
 
+        //Buttons
+        refreshButton = new JButton();
+        refreshButton.setText("REFRESH");
+        refreshButton.setFont(fontNormal);
+        refreshButton.setBackground(buttonBg);
+        refreshButton.setForeground(white);
+        refreshButton.setBounds(30, 600, 250, 60);
+        refreshButton.setFocusable(false);
+        refreshButton.addActionListener(this);
+
+        seeAllCarsButton = new JButton();
+        seeAllCarsButton.setText("See all cars");
+        seeAllCarsButton.setFont(fontNormal);
+        seeAllCarsButton.setBackground(buttonBg);
+        seeAllCarsButton.setForeground(white);
+        seeAllCarsButton.setBounds(330, 600, 250, 60);
+        seeAllCarsButton.setFocusable(false);
+        seeAllCarsButton.addActionListener(this);
+
+        exitButton = new JButton();
+        exitButton.setText("EXIT");
+        exitButton.setFont(fontNormal);
+        exitButton.setBackground(buttonBg);
+        exitButton.setForeground(white);
+        exitButton.setBounds(1080, 600, 150, 60);
+        exitButton.setFocusable(false);
+        exitButton.addActionListener(this);
+
+        checkButton = new JButton();
+        checkButton.setText("CHECK");
+        checkButton.setFont(fontNormal);
+        checkButton.setBackground(buttonBg);
+        checkButton.setForeground(white);
+        checkButton.setBounds(400, 10, 150, 60);
+        checkButton.setFocusable(false);
+        checkButton.addActionListener(this);
+
+        changeStatusButton = new JButton();
+        changeStatusButton.setText("CHANGE STATUS");
+        changeStatusButton.setFont(fontNormal);
+        changeStatusButton.setBackground(buttonBg);
+        changeStatusButton.setForeground(white);
+        changeStatusButton.setBounds(800, 10, 300, 60);
+        changeStatusButton.setFocusable(false);
+        changeStatusButton.addActionListener(this);
+
         //Creating JPanel
         JPanel displayPanel = new JPanel();
+        displayPanel.setLayout(null);
+        displayPanel.setBounds(30, 140, 1200,400);
+        displayPanel.setBackground(lightGreen);
 
-
+        //Input text
+        JTextField carId = new JTextField();
+        carId.setBackground(labelBg);
+        carId.setForeground(white);
+        carId.setHorizontalAlignment(JTextField.CENTER);
+        carId.setFont(fontNormal);
+        carId.setBounds(40, 10, 350, 60);
+        carId.setBorder(null);
 
         //Adding to frame
+        displayPanel.add(checkButton);
+        displayPanel.add(changeStatusButton);
+        displayPanel.add(carId);
+
+        frame.add(refreshButton);
+        frame.add(seeAllCarsButton);
+        frame.add(exitButton);
         frame.add(titleLabel);
         frame.add(allCars);
         frame.add(inOrder);
         frame.add(inProgress);
         frame.add(finished);
+        frame.add(displayPanel);
         frame.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        //Refresh button
+        if (e.getSource() == refreshButton) {
+            System.out.println("refresh");
+        }
 
+        //See all cars button
+        if (e.getSource() == seeAllCarsButton) {
+            System.out.println("see cars");
+        }
+
+        //Exit button
+        if (e.getSource() == exitButton) {
+            System.out.println("exit");
+        }
+
+        //Check button
+        if (e.getSource() == checkButton) {
+            System.out.println("check car");
+        }
+
+        //Change status
+        if (e.getSource() == changeStatusButton) {
+            System.out.println("change status");
+        }
     }
 }
